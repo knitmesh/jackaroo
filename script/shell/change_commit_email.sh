ae5f1934b21bc55879e2d5ded1b67403a10fe1c0
lijinghui <jingh.lib@gmail.com>
2019-04-19 18:11:27 +0800

    批量修改commit email

---
+++ script/shell/change_commit_email.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+git filter-branch --env-filter '
+OLD_EMAIL="lijinghui6@gomecloud.com"
+CORRECT_NAME="knitmesh"
+CORRECT_EMAIL="jingh.lib@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
